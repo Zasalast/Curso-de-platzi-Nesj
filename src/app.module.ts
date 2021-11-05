e240cf4ea800a1eebf07d900f103512afc73d014
zasalast <zasalas60@gmail.com>
2021-11-05 12:56:01 -0500

    Configuración por ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { DatabaseModule } from './database/database.module';
-
+import { enviroments } from './enviroments';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
     }),
     UsersModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('API_KEY') private apikey: string,
     @Inject('TASKS') private tasks: any[],
+    private config: ConfigService,
   ) {}
   getHello(): string {
-    console.log(this.tasks);
-    return `Hello World! ${this.apikey}`;
+    const apikey = this.config.get('API_KEY');
+    const dbkey = this.config.get<string>('DATABASE_NAME');
+    return `Hello World! ${apikey} ${dbkey}`;
   }
 }
