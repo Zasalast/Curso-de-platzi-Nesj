ddf67238feef52ca2fe276efcd1021cb3b2701cd
zasalast <zasalas60@gmail.com>
2021-11-06 16:29:16 -0500

    add service brands category, add dto brand category and add entities brands category

--- src/categories/categories.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CategoriesController } from './controllers/categories.controller';
-
-@Module({
-  imports: [],
-  controllers: [CategoriesController],
-  providers: [],
-})
-export class CategoriesModule {}
