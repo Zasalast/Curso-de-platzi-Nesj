a6159fe3f8858938840cbd70d69be88faa45a14e
DesarrolloColectivo <zasalastobon@gmail.com>
2021-11-06 17:33:29 -0500

    delete line import categori in module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { CategoriesModule } from './categories/categories.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
 import config from './config';
@@ -24,7 +23,7 @@ import config from './config';
     }),
     UsersModule,
     ProductsModule,
-    CategoriesModule,
+
     HttpModule,
     DatabaseModule,
   ],
