f9a54ac1f8d2fbb2f4d695bcb6ddefb536178810
zasalast <zasalas60@gmail.com>
2021-11-06 15:17:15 -0500

    Configuración de Heroku

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
